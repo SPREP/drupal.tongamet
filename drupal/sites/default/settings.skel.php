ac23cfdf8ac0e0e5a4809b307c36568e03830cfb
ainsofs <ainsof.soo@gmail.com>
2016-11-17 12:34:14 +1400

    update settings.skel.php file

--- drupal/sites/default/settings.skel.php
+++ drupal/sites/default/settings.skel.php
@@ -10,3 +10,19 @@ $databases['default']['default'] = array (
    'prefix' => '',
    'collation' => 'utf8mb4_general_ci',
 );
+/**
+ * Disable CSS and JS aggregation.
+ */
+# $config['system.performance']['css']['preprocess'] = FALSE;
+# $config['system.performance']['js']['preprocess'] = FALSE;
+/**
+ * Skip file system permissions hardening.
+ *
+ * The system module will periodically check the permissions of your site's
+ * site directory to ensure that it is not writable by the website user. For
+ * sites that are managed with a version control system, this can cause problems
+ * when files in that directory such as settings.php are updated, because the
+ * user pulling in the changes won't have permissions to modify files in the
+ * directory.
+ */
+$settings['skip_permissions_hardening'] = TRUE;
